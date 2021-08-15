8140fe9cb2c2b1e6acae0c3c80aedc317cb7816d
wenshengrui <wenshengr@163.com>
2021-08-15 23:21:28 +0800

    'week05-work'

---
+++ week05/go.mod
@@ -0,0 +1,3 @@
+module geektime-go/week05
+
+go 1.16
---
+++ week05/sliding_counter/sliding_counter_test.go
@@ -0,0 +1,74 @@
+package sliding_counter
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestMax(t *testing.T) {
+	testCase := []struct {
+		winTime    time.Duration
+		bucketTime time.Duration
+		data       []float64
+		want       float64
+	}{
+		{10 * time.Second, 1 * time.Second, []float64{10, 11, 9, 8, 12, 7, 6}, 12},
+	}
+
+	for _, tc := range testCase {
+		t.Run(fmt.Sprintf("winTime:%+v, bucketTime:%+v,", tc.winTime, tc.bucketTime), func(t *testing.T) {
+			n := NewNumber(tc.winTime, tc.bucketTime)
+			for _, x := range tc.data {
+				n.UpdateMax(x)
+				time.Sleep(1 * time.Second)
+			}
+			if got := n.Max(time.Now()); got != tc.want {
+				t.Errorf("got %f, want %f", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestAvg(t *testing.T) {
+	testCase := []struct {
+		winTime    time.Duration
+		bucketTime time.Duration
+		data       []float64
+		want       float64
+	}{
+		{10 * time.Second, 1 * time.Second, []float64{0.5, 1.5, 2.5, 3.5, 4.5}, 1.25},
+		{5 * time.Second, 500 * time.Millisecond, []float64{1, 2, 3, 4, 5}, 1.5},
+	}
+
+	for _, tc := range testCase {
+		name := fmt.Sprintf("winTime: %+v, bucketTime: %+v", tc.winTime, tc.bucketTime)
+		t.Run(name, func(t *testing.T) {
+			n := NewNumber(tc.winTime, tc.bucketTime)
+			for _, x := range tc.data {
+				n.Increment(x)
+				time.Sleep(tc.bucketTime)
+			}
+
+			if got := n.Avg(time.Now()); got != tc.want {
+				t.Errorf("got %f, want %f", got, tc.want)
+			}
+		})
+	}
+}
+
+func BenchmarkRollingNumberIncrement(b *testing.B) {
+	n := NewNumber(10*time.Second, 1*time.Second)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		n.Increment(1)
+	}
+}
+
+func BenchmarkRollingNumberUpdateMax(b *testing.B) {
+	n := NewNumber(10*time.Second, 1*time.Second)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		n.UpdateMax(float64(i))
+	}
+}
